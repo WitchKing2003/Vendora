36d64de02bb762fb9fa755363e01afadeca04922
WitchKing2003 <dinhnguyendev1123@gmail.com>
2026-09-15 22:13:08 +0700

    Feat: Update highlight slider

--- src/pages/components/Home/HomePage.tsx
+++ src/pages/components/Home/HomePage.tsx
@@ -1,10 +1,15 @@
-// import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import HighlightSlider from "./HighlightSlider/HighlightSlider";
 
 const HomePage = () => {
   const { t } = useTranslation();
 
-  return <div className="">{t("welcome")}</div>;
+  return (
+    <div className="">
+      <HighlightSlider />
+      <div className="px-4 py-10 text-center">{t("welcome")}</div>
+    </div>
+  );
 };
 
 export default HomePage;
